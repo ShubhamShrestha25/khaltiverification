87349b4f57d16751d2133345fea9ed4f22e54988
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-07-14 21:16:09 +0545

    heroku changes

--- index.js
+++ index.js
@@ -8,15 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", route);
 
-app.listen(process.env.PORT || 5000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+let port = process.env.PORT || 5000;
 
-app.listen(5000, function (error) {
+app.listen(port, function (error) {
   if (error) {
     console.log("SOMTHING WENT WRONG");
   } else {
