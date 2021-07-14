9afb3ce7e211b58364fd0a51ad48e3745b8bed52
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-07-14 21:06:32 +0545

    update

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", route);
 
+app.listen(process.env.PORT || 5000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
+
 app.listen(5000, function (error) {
   if (error) {
     console.log("SOMTHING WENT WRONG");
